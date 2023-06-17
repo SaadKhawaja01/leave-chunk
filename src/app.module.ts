8cdb4ca13de21e5d2710101a285cc034fa4f71b5
SaadKhawaja01 <khawajasaadmasood@gmail.com>
2023-06-18 03:08:12 +0500

    Feat: db cred changed

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,11 +9,11 @@ import { Allowed } from './leave/allowed.entity';
 //for Sql Db
 const DB = TypeOrmModule.forRoot({
   type: 'mysql',
-  host: 'containers-us-west-133.railway.app',
-  port: 7969,
+  host: 'localhost',
+  port: 3306,
   username: 'root',
-  password: 'AdV0KC7xXErt00gZpCU3',
-  database: 'railway',
+  password: '',
+  database: 'leaveChunk',
   entities: [Leave, Allowed],
   synchronize: true,
 });
